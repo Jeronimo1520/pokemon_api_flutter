fda14d22806d40f33f727522ec744f592f7380cb
Jerónimo Bedoya B <jeronimobedoya1520@gmail.com>
2024-04-22 07:27:46 -0500

    feat: Pokemon API controller

--- lib/controllers/PokemonController.dart
+++ lib/controllers/PokemonController.dart
@@ -5,12 +5,17 @@ import 'package:http/http.dart' as http;
 class PokemonController {
   final String url = "https://pokeapi.co/api/v2/pokemon";
 
-  Future<List<Pokemon>> getPokemons() async {
+  Future<List<Pokemon>> getPokemons({required int offset}) async {
     List<Pokemon> pokemons = [];
-    http.Response response = await http.get(Uri.parse(url));
-    Map<String, dynamic> json = jsonDecode(response.body);
+    Uri uri = Uri.parse("$url?offset=$offset&limit=20");
+    http.Response response = await http.get(Uri.parse(uri.toString()));
     if(response.statusCode != 200) {
       throw Exception("Error al consumir el API");
+    }
+    Map<String, dynamic> json = jsonDecode(response.body);
+    List results = json['results'] as List;
+    for(var result in results) {
+      pokemons.add(Pokemon(name: result['name'], url: result['url']));
     }
     return pokemons;
   }  
